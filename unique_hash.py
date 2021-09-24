dd3c74c35a8812a28786569bf305f3eff4bedd16
eneadodi <41304927+eneadodi@users.noreply.github.com>
2021-09-23 23:43:30 -0400

    first version

--- unique_hash.py
+++ unique_hash.py
@@ -2,43 +2,84 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 import matplotlib
-import numba as nb
+
 import random
 import collections 
 
 def graph(c,name):
+
     plt.bar(*zip(*c.items()))
     plt.savefig(name)
     plt.clf()
 
+def q2():
+    h_vals = []
+
+    t_vals = np.arange(1,20)
+
+    for j in t_vals:
+        h_vals = []
+        for i in range(10000):
+            h_vals.append(h(j,x=random.random()))
+        
+        counter_h1 = collections.Counter(h_vals)
+
+        graph(counter_h1,"h"+str(j)+".png")
+
+def q3():
+    h_vals_1 = [] 
+    h_vals_2 = []
+
+    t_vals_1 = [1,2,3,5,6,12,23,63]
+    t_vals_2 = [2,3,13,25,26,27,44,4]
+
+    collisions_dict = {}
+
+    for j in range(len(t_vals_1)):
+        collisions_counter = 0
+        for i in range(10000):
+            x = random.random()
+            h_vals_1.append(h(t_vals_1[j],x=x))
+            h_vals_2.append(h(t_vals_2[j],x=x))
+
+            if h_vals_1[i] == h_vals_2[i]:
+                collisions_counter+=1
+
+        collisions_dict["x="+str(t_vals_1[j])+","+str(t_vals_2[j])] = collisions_counter
+
+    graph(collisions_dict,"collisions_for_pairs.png")
+
+def q4():
+    h_vals = [] 
+
+    t_vals_1 = [1,2,3,5,6,12,23,63]
+    t_vals_2 = [2,3,13,25,26,27,44,4]
 
 
-def h(t,r=0.07):
-    x_new = random.random()
+
+    for j in range(len(t_vals_1)):
+        h_vals = []
+
+        for i in range(10000):
+            x = random.random()
+            h_vals.append(h(t_vals_1[j],x=x)-h(t_vals_2[j],x=x))
+
+        counter_h = collections.Counter(h_vals)
+
+        graph(counter_h,"h("+str(t_vals_1[j])+")-h("+str(t_vals_2[j])+").png")
+
+def h(t,r=0.02,x=random.random()):
+    x_new = x
     #y_new = random.random()
     for i in range(t):
         x_new = r*x_new*(1-x_new)# - s*(y_new*x_new)
         #y_new = r*y_new*(1-y_new) #-  s*(x_new*y_new)
     return int(np.floor(t/(x_new) % 100))
 
+
 def main():
-    data = h(1)
-    h_1 = []
-    h_2 = []
-    h_1minus2 = []
-
-    for i in range(10000):
-        h_1.append(h(1))
-        h_2.append(h(2))
-        h_1minus2.append((h_1[i]-h_2[i]))
-    
-    counter_h1 = collections.Counter(h_1)
-    counter_h2 = collections.Counter(h_2)
-    counter_h3 = collections.Counter(h_1minus2)
-
-    graph(counter_h1,"h1.png")
-    graph(counter_h2,"h2.png")
-    graph(counter_h3,"h1-h2.png")
-
+    #q2()
+    #q3()
+    q4()
 if __name__ == "__main__":
     main()
