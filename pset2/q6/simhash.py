727a5ac04be9593259be01f99c1aa3bccd6bb575
eneadodi <41304927+eneadodi@users.noreply.github.com>
2021-10-15 04:24:13 -0400

    finished creating for 1 table

--- pset2/q6/simhash.py
+++ pset2/q6/simhash.py
@@ -14,16 +14,16 @@ def load_pickle(picklename):
 def make_random_vector(shape):
     return np.random.normal(size=shape)
 
-class SimHash():
+class SimHashTable():
 
-    def __init__(self, min_cos_sim, table_repetitions, signature_length, input_shape):
-        self.min_cos_sim =min_cos_sim
-        self.t = table_repetitions
+    def __init__(self, min_cos_sim, signature_length, input_shape):
+        self.min_cos_sim = min_cos_sim
+        #self.t = table_repetitions
         self.r = signature_length
         self.input_shape = input_shape
     
         self.r_vectors = np.array([self.make_random_vector() for x in range(self.r)])
-        self.hash_tables = np.array([collections.defaultdict(list) for x in range(self.t)])
+        self.hash_table = collections.defaultdict(list) #np.array([collections.defaultdict(list) for x in range(self.t)])
 
 
     def make_random_vector(self):
@@ -38,28 +38,32 @@ class SimHash():
         else:
             return np.sign(np.sign(np.dot(v,self.r_vectors[specific_vector])))
         
-        return 
 
-    def generate_bit_code(self,v,all = False, specific_vector = 1):
+    def generate_bit_code(self,values,all = False, specific_vector = 1):
         def convert_to_bytes(x):
             if x == -1:
                 return '0'
             else:
                 return '1'
 
-        code = self.simhash(v,all=True)
-        print(code)
-        code = ''.join(map(convert_to_bytes,code))
-        return code
+        for v in values:
+            code = self.simhash(v,all=True)
+
+            code = ''.join(map(convert_to_bytes,code))
+            self.hash_table[hash(code)].append(v)
+            
+class SimHashFull():
+
+    def __init__(self,table_repetitions):
+        self.t = table_repetitions
+        self.tables = [SimHashTable() for x in range(self.t)]
 
             
-
-
 def main():
     mnist = load_pickle('data/mnist_normalized.pkl')
 
-    simh = SimHash(0.95,5,10,mnist['trainX'][0].shape)
-    print(simh.generate_bit_code(mnist['trainX'][0]))
+    simh = SimHashTable(0.95,10,mnist['trainX'][0].shape)
+    simh.generate_bit_code(mnist['trainX'])
 
 if __name__ == "__main__":
     main()
