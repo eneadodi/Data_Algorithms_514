f2ef17d6c1cf611414ea4430fc26d71315239b93
eneadodi <41304927+eneadodi@users.noreply.github.com>
2021-10-15 04:37:31 -0400

    done with repitions version!

--- pset2/q6/simhash.py
+++ pset2/q6/simhash.py
@@ -5,7 +5,7 @@ import scipy.io as sio
 from sklearn.preprocessing import Normalizer
 import numpy as np
 import collections 
-
+import time
 def load_pickle(picklename):
     with open(picklename,'rb') as inp:
         obj = pickle.load(inp)
@@ -39,7 +39,7 @@ class SimHashTable():
             return np.sign(np.sign(np.dot(v,self.r_vectors[specific_vector])))
         
 
-    def generate_bit_code(self,values,all = False, specific_vector = 1):
+    def generate_bit_codes(self,values):
         def convert_to_bytes(x):
             if x == -1:
                 return '0'
@@ -54,16 +54,37 @@ class SimHashTable():
             
 class SimHashFull():
 
-    def __init__(self,table_repetitions):
+    def __init__(self,min_cos_sim, signature_length,table_repetitions, input_shape):
+        self.min_cos_sim = min_cos_sim
         self.t = table_repetitions
-        self.tables = [SimHashTable() for x in range(self.t)]
+        self.r = signature_length
+        self.input_shape = input_shape
+        self.tables = [SimHashTable(self.min_cos_sim,self.r,self.input_shape) for x in range(self.t)]
 
-            
+    def fill_tables(self,values):
+        for i in range(self.t):
+            self.tables[i].generate_bit_codes(values)
+
+def checking_most(simh):
+    maxcount = max(len(v) for v in simh.hash_table.values())
+    most = [k for k,v in simh.hash_table.items() if len(v) == maxcount]
+    print(most[0])
+    print(len(simh.hash_table[most[0]]))
+
 def main():
     mnist = load_pickle('data/mnist_normalized.pkl')
 
-    simh = SimHashTable(0.95,10,mnist['trainX'][0].shape)
-    simh.generate_bit_code(mnist['trainX'])
+    #simh = SimHashTable(0.95,10,mnist['trainX'][0].shape)
+    #simh.generate_bit_codes(mnist['trainX'])
+    
+    # checking_most(simh) THIS WORKS!
+
+    start = time.time()
+    simhash_full = SimHashFull(0.95,10, 5, mnist['trainX'][0].shape)
+    simhash_full.fill_tables(mnist['trainX'])
+    end = time.time()
+
+    print('total time: ' ,(end-start), ' seconds :)')
 
 if __name__ == "__main__":
     main()
